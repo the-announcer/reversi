10592a51c3fdeeb5aec3c091ebc9296d66ee8fab
projalapeno <info@brinkley.tv>
2020-06-04 22:45:39 -0400

    split funcs into separate files

---
+++ printBoard.py
@@ -0,0 +1,26 @@
+def printBoard(b):
+	
+	width = range(len(b[0]))
+
+	print("  ", end="")
+	for n in width:
+		if n < max(width):
+			print(str(n+1) + " ", end="")
+		else:
+			print(str(n+1))
+	
+
+	for i in range(len(b)):
+		row = b[i]
+
+		y_ascii = i+65 	# capital A is 65
+		print(chr(y_ascii) + " " , end="")
+
+		for j in range(len(row)):
+			col = row[j]			
+			
+			if j < (len(row)-1):
+				print(col + "{}".format(" "), end="")
+			else:
+				print(col + "{}".format(" "))
+
