1adbe26b08b21f28322772b372a4b71cdc4a3c51
projalapeno <info@brinkley.tv>
2020-06-05 16:47:39 -0400

    changed compass values to int

--- inspectBoard.py
+++ inspectBoard.py
@@ -1,23 +1,21 @@
 def inspectBoard(marker, b, heading) -> tuple:
 
-
-    heading = heading.lower()
-    
     x = marker[0]
     y = marker[1]
     p = marker[2]
 
     compass = {
-        'nw': (x-1, y-1, p),
-        'n': (x, y-1, p),
-        'ne': (x+1, y-1, p),
-        'w': (x-1, y, p),
-        'e': (x+1, y, p),
-        'sw': (x-1, y+1, p),
-        's': (x, y+1, p),
-        'se': (x+1, y+1, p)
+        '1': (x-1, y-1, p),
+        '2': (x, y-1, p),
+        '3': (x+1, y-1, p),
+        '4': (x-1, y, p),
+        '6': (x+1, y, p),
+        '7': (x-1, y+1, p),
+        '8': (x, y+1, p),
+        '9': (x+1, y+1, p)
     }
 
+    ## we will check this direction on the board for an opposite player piece/marker
     target = compass[heading]
             
     dst_x = target[0]
